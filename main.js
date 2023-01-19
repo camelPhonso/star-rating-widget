aa65a55cae7329e581aaeb92084a6f653750a4ab
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-01-19 09:06:34 +0000

    added display for users rating

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const stars = document.querySelectorAll('.fa-star')
+const showRating = document.getElementById('user-rating_input');
 
 function rate(){
+    stars.forEach(star => star.classList.remove('checked'));
     this.classList.add('checked');
+    display();
     for(i = 0; i < stars.length; i++){
         if(stars[i].classList.contains('checked')){
             break;
         }else{
             stars[i].classList.add('checked');
+        }
+    }
+}
+
+function display(){
+    for(let i = 0; i , stars.length; i++){
+        if(stars[i].classList.contains('checked')){
+            showRating.innerText = i+1;
+            break;
         }
     }
 }
